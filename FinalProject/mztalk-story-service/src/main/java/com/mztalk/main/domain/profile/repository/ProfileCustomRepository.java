e5e03a49bce6238417092fa653cb680ce309a6bb
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-03-20 16:20:52 +0900

    feat 파이널 프로젝트 리팩토링

---
+++ FinalProject/mztalk-story-service/src/main/java/com/mztalk/main/domain/profile/repository/ProfileCustomRepository.java
@@ -0,0 +1,11 @@
+package com.mztalk.main.domain.profile.repository;
+
+
+import com.mztalk.main.domain.profile.entity.Profile;
+
+import java.util.Optional;
+
+public interface ProfileCustomRepository {
+
+
+}
